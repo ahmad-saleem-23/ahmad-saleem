efa6f40c36e083ad75b6ea4a1a9ba8d8c6a1c471
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-10-04 14:21:53 +1300

    update Hero element

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,10 +1,28 @@
-import { motion } from "framer-motion";
-import {styles} from './styles'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
 
-const Hero =()=>{
-  return(
-    <section className='relativ w-full h-screen mx-auto p-top-20'>
-      Hero</section>
+const Hero = () => {
+  return (
+    <section className="relativ w-full h-screen mx-auto pt-40">
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5 ">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div className="">
+          <h1 className={`${styles.heroHeadText}`}>
+            {' '}
+            Hi, I'm <span className="text-[#915eff]">Ahmad</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a junior software developer, <br className="sm:block hidden" />I
+            love working with fullstack apps, and building new stuff.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 export default Hero
