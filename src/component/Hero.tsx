999b0397480244b190b86317ec1debdcb40d2a69
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-10-04 23:40:30 +1300

    adding computer model

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,9 +1,10 @@
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
+import ComputersCanvas  from "./canvas/computers";
 
 const Hero = () => {
   return (
-    <section className="relativ w-full h-screen mx-auto pt-40">
+    <section className="relativ w-full h-screen mx-auto pt-60">
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -11,9 +12,9 @@ const Hero = () => {
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
-        <div className="">
+        <div>
           <h1 className={`${styles.heroHeadText}`}>
-            {' '}
+          
             Hi, I'm <span className="text-[#915eff]">Ahmad</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
@@ -21,6 +22,27 @@ const Hero = () => {
             love working with fullstack apps, and building new stuff.
           </p>
         </div>
+      
+      </div>  
+      
+      <ComputersCanvas />
+   
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
       </div>
     </section>
   )
---
+++ src/component/canvas/computers.tsx
@@ -0,0 +1,56 @@
+import { Suspense} from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF, Stage } from "@react-three/drei";
+import CanvasLoader from "../CanvasLoader";
+
+const ComputerModel = () => {
+  const computer = useGLTF("./computers/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={0.5}
+        position={[0, -3.25, -1.5]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [0.2, 0.1, 0.3], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+       <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls
+        enableZoom={true}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+        dampingFactor={0.3}
+      />
+       
+      <Stage contactShadow={{ resolution: 1024, scale: 1000 }}>
+       <ComputerModel/>
+      </Stage>
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
