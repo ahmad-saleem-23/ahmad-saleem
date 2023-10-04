9674326cc82d5107e2120c4b200a6aa02fbaec86
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-10-05 01:48:46 +1300

    adding a horizontal scroll loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-
 import AboutMe from './component/AboutMe'
 import Logo from './component/Logo'
 import { Nav } from './component/menu/Nav'
 import Contacts from './component/Contacts'
-import  Hero  from './component/Hero'
-
-
-
+import Hero from './component/Hero'
+import HorizontalScrollLoop from './component/HorizontalScroll'
 
 const App = () => {
   return (
     <>
-    <div className="realative z-0 bg-primary ">
-    
-      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center  '>
-      <Logo />
-      <Nav />
-      <Hero />
-  
-
-      
-
-      
+      <div className="realative z-0 bg-primary ">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center  ">
+          <Logo />
+          <Nav />
+          <Hero />
+        </div>
+        <AboutMe />
+        <HorizontalScrollLoop/>
+        <div className="relative z-0">
+          <Contacts />
+        </div>
       </div>
-      <AboutMe />
-      <div className='relative z-0'>
-      <Contacts />
-      </div>
-    </div>
     </>
   )
 }
