6f9d4ffa88c47e56d8b98e7bed89972c1685681e
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-10-11 14:52:11 +1300

    adjust code for mobile

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,7 +1,9 @@
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
-import ComputersCanvas  from "./canvas/computers";
+// import ComputersCanvas  from "./canvas/computers";
+import { lazy } from 'react'
 
+const DynamicComputersCanvas = lazy(() => import('./canvas/computers'))
 const Hero = () => {
   return (
     <section className="relativ w-full h-screen mx-auto pt-60">
@@ -14,7 +16,6 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}`}>
-          
             Hi, I'm <span className="text-[#915eff]">Ahmad</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
@@ -22,14 +23,15 @@ const Hero = () => {
             love working with fullstack apps, and building new stuff.
           </p>
         </div>
+      </div>
+
       
-      </div>  
-      
-      <ComputersCanvas />
+        <DynamicComputersCanvas />
    
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#aboutMe'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#aboutMe">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -37,9 +39,9 @@ const Hero = () => {
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: "loop",
+                repeatType: 'loop',
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Checker from 'vite-plugin-checker'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Checker({ typescript: true })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          // Other dependencies or modules
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust this value as needed
+  },
 })
